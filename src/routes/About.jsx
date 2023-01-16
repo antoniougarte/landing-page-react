45f38c31a06fa94553a6cd5f500a174f29f97067
antoniougarte <antoniougartemendoza@gmail.com>
2023-01-16 01:33:52 -0500

    cambios en el vite config

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -9,8 +9,7 @@ function About() {
     <>
       <Header/>
       <Hero 
-    title="Learn more about us
-" 
+    title="Learn more about us" 
     description="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
     image={new4}/>
     <AboutCards/>
